7896dcd906697a0542f30ce3bc0ff5d2acb33db4
tbreuss <thomasbreuss@gmx.ch>
2022-01-26 06:26:56 +0100

    chore: improve start script

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
-# start frontend
-npm run dev --prefix frontend
+#!/bin/bash
 
-# start backend
-ENVIRONMENT=development php -d variables_order=EGPCS -S localhost:7000 -t backend/public
+# start backend in background and store process id
+ENVIRONMENT=development php -d variables_order=EGPCS -S localhost:7000 -t backend/public & pids=$!
+
+# start frontend in background and store process id
+npm run dev --prefix frontend & pids+=" $!"
+
+# set up trap and kill processes in case CTRL-C is hit
+trap "kill $pids" SIGTERM SIGINT
+
+# block until both processes have finished
+wait $pids
